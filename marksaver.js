ace5443ef71a4eb63edc627e7580ad37bac9a1bd
Claire Parker <claire.parker87@gmail.com>
2016-07-20 21:50:55 +0100

    :baggage_claim: Naively works for happy path

--- marksaver.js
+++ marksaver.js
@@ -1,33 +1,41 @@
 function saveValue() {
     console.log("Getting there");
-    // Get a value saved in a form.
-    var theValue = document.getElementById('mark').value;
-    // Check that there's some code there.
-    if (!theValue) {
-        printMessage('Error: No value specified');
-        return;
-    }
-    // Save it using the Chrome extension storage API.
-    chrome.storage.sync.set({
-        'value': theValue
-    }, function() {
-        // Notify that we saved.
-        printMessage('Settings saved');
+    // Get the current bookmarks
+    chrome.tabs.query({
+        "currentWindow": true
+    }, function(tabs) {
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({
+            'value': tabs
+        }, function() {
+            // Notify that we saved.
+            printMessage('Settings saved');
+        });
+    });
+}
+
+function getTabUrls(arrayOfTabs) {
+    return arrayOfTabs.map(function(urlObj) {
+        return urlObj.url;
     });
 }
 
 function loadValue() {
-    chrome.storage.sync.get('value', function(theValue) {
-        printMessage("Result is: " + theValue.value);
-        console.log(theValue.value);
+    chrome.storage.sync.get('value', function(tabs) {
+        printMessage("Result is: " + tabs.value[0].url + "<br />There are " + tabs.value.length + " links");
+        var tabUrls = getTabUrls(tabs.value);
+        console.log(tabUrls);
+        for (var i = 0; i < tabUrls.length; i++) {
+            chrome.tabs.create({url : tabUrls[i]});
+        }
     });
 }
 
 function printMessage(msg) {
     document.getElementById('results').innerHTML = msg;
 }
+
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("We're loaded");
     document.getElementById('save-me').addEventListener('click', saveValue);
     document.getElementById('load-me').addEventListener('click', loadValue);
 });
